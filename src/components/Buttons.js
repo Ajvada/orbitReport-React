ec65ab0f0659061476cdd8fdf17512a66ec7f781
vada <138933714+Ajvada@users.noreply.github.com>
2023-08-18 16:59:28 -0400

    done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,64 @@
-const Buttons = () => {
+import React from 'react';
+import satData from './satData'; 
+import '../App';
+import './styling.css'
+const Buttons = (props) => {
+  console.log('Display', props.displaySats)
+
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
-  );
+    <div className='flex-container'>
+      {props.displaySats.map((sat, id ) => {
+      
+        
+        return(
+        <button onClick={() => props.filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+        );
+        })}
+
+      
+      <button onClick={() => props.setSat(satData)}>All Orbits</button>
+    </div>
+  )
 };
 
 export default Buttons;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Buttons = () => {
+//   return (
+//     <div>
+//       <button>Placeholder Button</button>
+//       <button>All Orbits</button>
+//       </div>
+//   );
+// };
+
+// export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,84 @@
-const Table = ({ sat }) => {
+import React from 'react';
+import './styling.css'
+const Table = ({ sat }) => { 
+  console.log(sat.map)
   return (
-      <table>
-       <thead>
+    <table>
+      <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
-      </table>
+      </thead>
+      <tbody>
+        {sat.map(({ id, name, type, launchDate, operational }) => (
+          <tr key={id}>
+            <td>{name}</td>
+            <td>{type}</td>
+            <td>{launchDate}</td>
+            <td>{operational ? 'Active' : 'Inactive'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Table = ({ sat }) => {
+//   return (
+//       <table>
+//        <thead>
+//         <tr>
+//           <th>Header TBD</th> 
+//           <th> </th> 
+//           <th> </th> 
+//           <th> </th> 
+//         </tr>
+//         </thead>
+//         <tbody>
+//         <tr>
+//           <td>Row Data TBD</td>
+//         </tr>
+//         </tbody>
+//       </table>
+//   );
+// };
+
+// export default Table;
